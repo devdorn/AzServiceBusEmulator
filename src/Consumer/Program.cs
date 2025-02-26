8587d7e1d9cac11d4c29f070d8d1f8034bfccf54
Bernhard Dorn <mail@bernharddorn.de>
2025-02-26 16:33:34 +0100

    Add project files.

---
+++ src/Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string _connectionString = "Endpoint=sb://127.0.0.1;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
+const string queueName = "queue.1e";
+
+var client = new ServiceBusClient(_connectionString);
+
+var serviceBusReceiverOptions = new ServiceBusReceiverOptions
+{
+    ReceiveMode = ServiceBusReceiveMode.PeekLock,
+};
+
+var receiver = client.CreateReceiver(queueName, serviceBusReceiverOptions);
+
+try
+{
+    Console.WriteLine("Receiving messages...");
+
+    while (true)
+    {
+        var message = await receiver.ReceiveMessageAsync(TimeSpan.FromSeconds(5));
+
+        if (message != null)
+        {
+            Console.WriteLine($"Received message: {message.Body}");
+            await receiver.CompleteMessageAsync(message);
+
+            {
+                Console.WriteLine($"Received message: {message.Body}");
+
+                await receiver.CompleteMessageAsync(message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No messages received.");
+            break;
+        }
+    }
+}
+finally
+{
+    await receiver.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
---
+++ src/Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Messaging.ServiceBus;
+
+
+const string _connectionString = "Endpoint=sb://127.0.0.1;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
+const string _queueName = "queue.1";
+
+const int numOfMessages = 3;
+
+var client = new ServiceBusClient(_connectionString);
+
+var sender = client.CreateSender(_queueName);
+
+using var messageBatch = await sender.CreateMessageBatchAsync();
+
+for (var i = 1; i <= numOfMessages; i++)
+{
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+    {
+        throw new Exception($"The message {i} is too large to fit in the batch.");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A batch of {numOfMessages} messages has been published.");
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
